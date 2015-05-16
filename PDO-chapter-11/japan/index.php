e061d949a0cba33769f9b76dc9e28fc887ad6bb6
dan-stachlewski <dan.stachlewski@gmail.com>
2015-05-16 17:53:12 +1000

    Decided to create my own version of the Example from Chapter 11 called websites. So I am putting all the original code (japan) into a Japan folder and all my code (website) in the Websites folder.

---
+++ PDO-chapter-11/japan/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" />
+<head>
+	<meta charset="utf-8" />
+	<meta name="author" content="Dan Stachlewski" />
+	<meta name="description" content=" " />
+	<meta name="keywords" content=" " />
+	<title>MySQLi &amp; PDO:</title>
+</head>
+<body>
+<h1>MySQLi &amp; PDO Examples</h1>
+<p>The Links below take you to files that have different ways of connecting to the 'phpsols' database and retrieving different types of information.</p>
+<p>MySQLi Connection and ALL(*) data retrieval. <a href="mysqli.php">MySQLi</a></p>
+<p>PDO Connection and ALL(*) data retrieval. <a href="pdo.php#all">PDO</a></p>
+<p>PDO Connection and filename, caption data retrieval. <a href="pdo.php#fileNameCaption">PDO</a></p>
+<p>PDO Connection and ORDER BY caption data retrieval. <a href="pdo.php#orderByCaption">PDO</a></p>
+<p>PDO Connection and ORDER BY caption DESCENDING data retrieval. <a href="pdo.php#orderByCaptionDesc">PDO</a></p>
+<p>PDO Connection and SEARCH BY image_id = 6 data retrieval. <a href="pdo.php#searchByImageID6">PDO</a></p>
+<p>PDO Connection and SEARCH BY image_id > 4 data retrieval. <a href="pdo.php#searchByImageID4">PDO</a></p>
+<p>PDO Connection and SEARCH LIKE Kyoto retrieval. <a href="pdo.php#searchLikeKyoto">PDO</a></p>
+
+<p>PDO Connection and SEARCH LIKE Maiko data retrieval. <a href="pdo.php#searchLikeMaiko">PDO</a></p>
+<p>PDO Connection and SEARCH LIKE (BINARY = CASE SENSITIVE) Maiko data retrieval. <a href="pdo.php#searchLikeBinaryMaiko">PDO</a></p>
+
+
+
+
+</body>
+</html>
